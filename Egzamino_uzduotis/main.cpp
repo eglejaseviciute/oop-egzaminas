ba9122359f1add6d6a6d1ab68150c5b0f57b3645
eglejaseviciute <eglejasev@gmail.com>
2024-12-19 21:22:26 +0200

    Patvarkytas README.md failas

--- Egzamino_uzduotis/main.cpp
+++ Egzamino_uzduotis/main.cpp
@@ -3,7 +3,11 @@
 
 int main() {
     try {
-
+        #ifdef _WIN32
+        SetConsoleOutputCP(CP_UTF8);
+        SetConsoleCP(CP_UTF8);
+        #endif
+        
         TekstoAnalizatorius analizatorius("tekstas.txt");
 
         // Rezultatu failo sukurimas
